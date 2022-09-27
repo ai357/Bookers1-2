b8de99f5eb055bc242971ab8f0e25812640c94b1
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2022-09-27 09:34:20 +0000

    9/26

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
-  
+
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -12,4 +15,16 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
