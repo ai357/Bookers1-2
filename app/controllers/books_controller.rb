ee301baf2e12b6cff2e166cafd8a4ad31a533b40
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2022-09-29 07:10:12 +0000

    9/29

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,16 +11,37 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
+
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+       @books = Book.all
+       render :index
+
+    end
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
+        redirect_to book_path(book.id), notice: "Book was successfully updated."
+    else
+        render :edit
+    end
+  end
+
 
   private
   def book_params
